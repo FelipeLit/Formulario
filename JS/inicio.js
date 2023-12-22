50575b47a329014a18c277779f3111fa121e0f46
Felipe Gallego <114832482+FelipeLit@users.noreply.github.com>
2023-12-22 15:49:46 -0500

    Add files via upload

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,22 +1,34 @@
-let genero = sessionStorage.getItem("Genero")
+let genero = sessionStorage.getItem("Genero");
+
+let ruta = "../img/"
+
+let classImages= document.getElementsByClassName("imagen");
+
+
+
 if (genero === "m"){
     document.body.classList.add("masculino")
     document.body.classList.remove("femenino")
+    let imgenesMasculinas = ["pizza.jpg", "pizza2.jpg", "pizza3.jpg", "pizza4.jpg"]
+    
+    let i = 0;
+    for (img of classImages){
+        //console.log(img); elemento de html
+        img.src = ruta+imgenesMasculinas[i];
+        i++;
+    };
 }
+
 if(genero === "f"){
-    document.body.classList.add("femenino")
-    document.body.classList.remove("masculino")
+    document.body.classList.add("femenino");
+    document.body.classList.remove("masculino");
+
+    let imgenesFemeninas = ["mujer1.jpg", "mujer2.jpg", "mujer3.jpg", "mujer4.jpg"]
+    let j = 0;
+
+    for(img of classImages){
+        img.src = ruta+imgenesFemeninas[j];
+        j++;
+    }
 }
 
-let imgenesMasculinas = ["pizza.jpg", "pizza2.jpg", "pizza3.jpg", "pizza4.jpg"]
-let ruta = "../img/"
-
-let classImages= document.getElementsByClassName("imagen")
-
-let i = 0;
-
-for (img of classImages){
-    //console.log(img); elemento de html
-    img.src = ruta+imgenesMasculinas[i];
-    i++;
-};
